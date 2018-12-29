f7b23a40039af7c7bea400f968988948dce839a5
Angelo Camillo <angelocamillo@hotmail.com>
2018-12-29 17:52:26 -0200

    Started 0.1218a

---
+++ src/main/java/com/camillo/astrallis/Astrallis01218Application.java
@@ -0,0 +1,14 @@
+package com.camillo.astrallis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Astrallis01218Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Astrallis01218Application.class, args);
+	}
+
+}
+
